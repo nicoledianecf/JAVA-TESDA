bc0ec31db17194ca33421095df2b3c1c9e1988be
nicoledianecf <nicoleformeloza23@gmail.com>
2024-12-11 05:34:49 +0000

    Done task112

--- chapter1/Tasksheet112.java
+++ chapter1/Tasksheet112.java
@@ -1,1 +1,17 @@
+public class Tasksheet112 {
 
+    public static void main(String[] args) {
+
+        byte zero = 0;
+        short one = 1;
+        int letterToNum = 311;
+        float twoZero = 2.0f;
+        char letterD = 'd';
+        boolean b = true;
+
+        String output = "H" + letterToNum + zero + " " + "w" + zero + "r" + one + letterD + " " + twoZero + " " + b;
+
+        System.out.println(output);
+
+    }
+}
