f2924f092411229a07ab18dbf2accb306efc10bc
nicoledianecf <nicoleformeloza23@gmail.com>
2024-12-11 06:04:20 +0000

    Done 113

--- chapter1/Tasksheet113.java
+++ chapter1/Tasksheet113.java
@@ -1,1 +1,28 @@
+public class Tasksheet113 {
 
+
+      public static void main (String[] args){
+
+        int a = 1;
+        int b = 2;
+
+        int result = a + b; // result is now 3
+        System.out.println(result);
+
+        result -= 1; // result is now 2
+        System.out.println(result);
+
+        result *= 2; // result is now 4
+        System.out.println(result);
+
+        result /= 2; // result is now 2
+        System.out.println(result);
+
+        result += 8; // result is now 10
+        System.out.println(result);
+
+        result %= 7; // result is now 3
+        System.out.println(result);
+
+    }
+}
